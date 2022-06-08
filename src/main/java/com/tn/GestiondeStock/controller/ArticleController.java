b39c6a24975cd74ffc820ae8a5ab5a1d8f049517
AchrefMahmoud <78664859+AchrefMahmoud@users.noreply.github.com>
2022-06-08 11:58:45 +0200

    commit

--- src/main/java/com/tn/GestiondeStock/controller/ArticleController.java
+++ src/main/java/com/tn/GestiondeStock/controller/ArticleController.java
@@ -1,5 +1,55 @@
 package com.tn.GestiondeStock.controller;
 
-public class ArticleController {
+import com.tn.GestiondeStock.controller.api.ArticleApi;
+import com.tn.GestiondeStock.dto.ArticleDto;
+import com.tn.GestiondeStock.services.ArticleService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleController implements ArticleApi  {
+
+	private ArticleService articleService;
+	
+	@Autowired
+	public ArticleController(ArticleService articleService)  {
+		this.articleService = articleService; 
+	}
+	
+	@Override
+	public ArticleDto save(ArticleDto dto) {
+		// TODO Auto-generated method stub
+		return articleService.save(dto);
+	}
+
+	@Override
+	public ArticleDto findById(Integer id) {
+		// TODO Auto-generated method stub
+		return articleService.findById(id);
+	}
+
+	@Override
+	public ArticleDto findByCodeArticle(String codeArticle) {
+		// TODO Auto-generated method stub
+		return articleService.findByCodeArticle(codeArticle);
+	}
+
+	@Override
+	public List<ArticleDto> findAll() {
+		// TODO Auto-generated method stub
+		return articleService.findAll();
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		articleService.delete(id);
+	}
+	
+	
 
 }
--- src/main/java/com/tn/GestiondeStock/controller/api/ArticleApi.java
+++ src/main/java/com/tn/GestiondeStock/controller/api/ArticleApi.java
@@ -3,6 +3,7 @@ package com.tn.GestiondeStock.controller.api;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +19,16 @@ public interface ArticleApi {
 	@PostMapping(value = APP_ROOT + "/articles/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	ArticleDto save (@RequestBody ArticleDto dto);
 	
-	@GetMapping(value = APP_ROOT + "/articles/idArticle", produces = MediaType.APPLICATION_JSON_VALUE)
-	ArticleDto findById (@PathVariable Integer id);
+	@GetMapping(value = APP_ROOT + "/articles/{idArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
+	ArticleDto findById (@PathVariable("idArticle") Integer id);
 	
-	ArticleDto findByCodeArticle (String codeArticle);
-	
+	@GetMapping(value = APP_ROOT + "/articles/{codeArticle}", produces = MediaType.APPLICATION_JSON_VALUE)
+	ArticleDto findByCodeArticle (@PathVariable("codeArticle") String codeArticle);
+
+	@GetMapping(value = APP_ROOT + "/articles/all", produces = MediaType.APPLICATION_JSON_VALUE)
 	List<ArticleDto> findAll();
 	
-	void delete(Integer id );
+	@DeleteMapping(value = APP_ROOT + "/articles/delete/{idArticle}")
+	void delete( @PathVariable("idArticle") Integer id );
 	
 }
